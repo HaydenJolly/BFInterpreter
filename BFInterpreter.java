dc2fbf1f6b0687ce71de507eb2ffa923bd70889d
Your Name <you@example.com>
2019-02-24 19:29:32 -0700

    formatting

--- BFInterpreter.java
+++ BFInterpreter.java
@@ -1,6 +1,6 @@
 public class BFInterpreter
 {
-    private String instructions; 
+    private String instructions;
     private char instruction;
     private byte [] mem;
     private int memIndex, instructionIndex, instructionEnd;
@@ -10,80 +10,54 @@ public class BFInterpreter
         instructions = _instructions;
         mem = new byte [30000];
         memIndex = 0;
-        instructionIndex = 0;
+        instructionIndex = -1;
         instructionEnd = instructions.length();
     }
 
     public void runProgram()
     {
         //iterate over all instructions
-        while(instructionIndex < instructionEnd)
+        while(++instructionIndex < instructionEnd)
         {
-            instruction = instructions.charAt(instructionIndex);
-            switch (instruction)
-            {
-                //increments memory at memIndex (wraps around to 0 after 256)
-                case '+':
+            switch (instructions.charAt(instructionIndex)){
+                case '+': //increments memory at memIndex (wraps around to 0 after 256)
                 mem[memIndex] = (byte)(++mem[memIndex] % 256);
                 break;
-                
-                //decrement memory at memIndex (wraps to 255 below 0)
-                case '-':
-                if (mem[memIndex] > 0)
-                {
-                    mem[memIndex]--;
-                }else
-                {
-                    mem[memIndex] = (byte)255;
-                }
-                break;
-                
-                //points memIndex one unit forward (wraps back to 0)
-                case '>':
-                memIndex = ++memIndex % 30000;
-                break;
 
-                //points memIndex one unit backwards (wraps to end)
-                case '<':
-                if (memIndex > 0)
-                {
-                    memIndex--;
-                }else
-                {
-                    memIndex = 29999;
-                }
-                break;
+                case '-': //decrement memory at memIndex (wraps to 255 below 0)
+                  mem[memIndex] = (mem[memIndex] > 0) ? mem[memIndex] - 1 : (byte)255;
+                  break;
 
-                //checks loop conidition and seeks loop close
-                case '[':
-                if (mem[memIndex] == 0)
-                {
-                    instructionIndex = findLoopClose();
-                }
-                break;
+                case '>': //points memIndex one unit forward (wraps back to 0)
+                  memIndex = ++memIndex % 30000;
+                  break;
 
-                //seeks loop open
-                case ']':
-                instructionIndex = findLoopOpen();
-                break;
+                case '<': //points memIndex one unit backwards (wraps to end)
+                  memIndex = (memIndex > 0) ? memIndex - 1 : 29999;
+                  break;
 
-                //outputs the ascii character associated with current memory value
-                case '.':
-                System.out.print((char)mem[memIndex]);
-                break;
-                    
-                //sets the current memory location to the value of an ascii character input
-                case ',':
-                System.out.print("\nEnter an ASCII character whose value will be stored in the current cell: ");
-                mem[memIndex] = (byte)in.next().charAt(0);
-                in.nextLine();
-                break;
+                case '[': //checks loop conidition and seeks loop close
+                  if (mem[memIndex] == 0) instructionIndex = findLoopClose();
+                  break;
+
+                case ']': //seeks loop open
+                  instructionIndex = findLoopOpen();
+                  break;
+
+                case '.': //outputs the ascii character associated with current memory value
+                  System.out.print((char)mem[memIndex]);
+                  break;
+
+                case ',': //sets the current memory location to the value of an ascii character input
+                  System.out.print("\nEnter an ASCII character whose value will be stored in the current cell: ");
+                  mem[memIndex] = (byte)in.next().charAt(0);
+                  in.nextLine();
+                  break;
             }
-            instructionIndex++;
         }
     }
 
-    public int findLoopClose()
+    private int findLoopClose()
     {
         int loopOpens = 0;
         boolean foundEnd = false;
@@ -92,24 +66,16 @@ public class BFInterpreter
         while (!foundEnd)
         {
             localInstruction = instructions.charAt(localInstructionIndex);
-            if (localInstruction == '[')
-            {
-                loopOpens++;
-            }
-            else if (localInstruction == ']')
-            {
-                loopOpens--;
-                if (loopOpens == 0)
-                {
-                    foundEnd = true;
-                }
-            }
+            if (localInstruction == '[') loopOpens++;
+            else if (localInstruction == ']') loopOpens--;
+
+            foundEnd = loopOpens == 0 && localInstruction == ']';
             localInstructionIndex++;
         }
         return --localInstructionIndex;
     }
 
-    public int findLoopOpen()
+    private int findLoopOpen()
     {
         int loopEnds = 0;
         boolean foundOpen = false;
@@ -118,18 +84,10 @@ public class BFInterpreter
         while (!foundOpen)
         {
             localInstruction = instructions.charAt(localInstructionIndex);
-            if (localInstruction == ']')
-            {
-                loopEnds++;
-            }
-            if (localInstruction == '[')
-            {
-                loopEnds--;
-            }
-            if (loopEnds == 0 && localInstruction == '[')
-            {
-                foundOpen = true;
-            }
+            if (localInstruction == ']') loopEnds++;
+            else if (localInstruction == '[') loopEnds--;
+
+            foundOpen = loopEnds == 0 && localInstruction == '[';
             localInstructionIndex--;
         }
         return localInstructionIndex;
--- README.md
+++
@@ -1,2 +0,0 @@
-# BFInterpreter
-This repository is mostly just to test out github
